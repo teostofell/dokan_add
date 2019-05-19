4625f976d3394c8bca3ade2234c70574d47f76f5
Ivan Chychyn <ichychyn@merkleinc.com>
2019-05-19 18:17:59 +0300

    API updates

--- src/utils/API.js
+++ src/utils/API.js
@@ -7,7 +7,20 @@ const defaultOptions = {
     },
 };
 
-
+function defineCategory(cat){
+    switch(cat){
+        case 'sneakers_new':
+            return { name: "Sneakers", condition: "New" };
+        case 'sneakers_used':  
+            return { name: "Sneakers", condition: "Used" };
+        case 'apparel_new':
+            return { name: "Apparel", condition: "New" };
+        case 'apparel_used':
+            return { name: "Apparel", condition: "Used" };
+        default:
+            return { name: "Sneakers", condition: "New" };
+    }
+}
 
 async function getAttributes(){
     let attributes  = await fetch(`${API_PATH}/products/attributes`, defaultOptions);
@@ -92,10 +105,13 @@ async function createProduct(data){
 
         // Create product
         let product = {};
+        let category = defineCategory(data.category);
+
 
         product.name = data.model_line;
+        product.type = "variable";
         product.categories = [{
-            name: 'Uncategorized'
+            name: category.name,
         }];
         product.attributes = [];
 
@@ -103,10 +119,18 @@ async function createProduct(data){
             name: 'brand',
             options: data.brand,
         });
-
+        product.attributes.push({
+            name: 'condition',
+            options: category.condition,
+        });
         product.attributes.push({
             name: 'colorway',
             options: data.colors.map(c => c.name),
+        });
+        product.attributes.push({
+            name: 'size',
+            options: data.sizes.map(s => s.name),
+            variation: true,
         });
         product.description = data.description;
         product.attributes.push({
@@ -133,6 +157,28 @@ async function createProduct(data){
         product = await response.json();
 
         // Create variations
+        for(const size of data.sizes){
+            let variation = {};
+            variation.attributes = [];
+            variation.regular_price = size.price;
+            variation.stock_quantity = size.quantity;
+            variation.attributes.push({
+                name: 'size',
+                option: size.name,
+            });
+            variation.manage_stock = true;
+
+            const response = await fetch(`${API_PATH}/products/${product.id}/variations`, {
+                method: 'POST',
+                headers: {
+                    ...defaultOptions.headers,
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(variation)
+            });
+            variation = await response.json();
+        }
 
         return product;
     } catch(e){
